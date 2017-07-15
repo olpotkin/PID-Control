6241cb3be6374d0c064ba0a1a5c61b6c41951e30
Oleg Potkin <olpotkin@gmail.com>
2017-07-15 13:47:03 -0700

    feat: add PID control essentials

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,32 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+    // Initialize PID controller with the coefficients as the input values.
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
 }
+
 
 void PID::UpdateError(double cte) {
+
+    // Difference from the previous cte (p_error) to the new cte
+    d_error = (cte - p_error);
+
+    // Set to the new cte
+    p_error = cte;
+
+    // Sum of cte's at this point
+    i_error += cte;
 }
 
+
 double PID::TotalError() {
+
+    // Total error of each coefficient multiplied by the respective error
+    double t_error = -Kp * p_error - Kd * d_error - Ki * i_error;
+
+    return t_error;
 }
 
